8f994e09ff350fc0ea98526c4e35e70b8a566c8a
sahwaabu <70576401+sahwaabu@users.noreply.github.com>
2023-11-29 19:18:04 +0530

    women entreprenurship faq
    women entreprenurship faq

--- script.js
+++ script.js
@@ -1,100 +1,24 @@
-window.onscroll = function() {myFunction()};
 
-var nav = document.getElementById("nav");
-var sticky = nav.offsetTop;
+    const wepAccordionFaqItemHeaders = document.querySelectorAll(".wep-accordion-faq-item-header");
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    nav.classList.add("sticky")
-  } else {
-    nav.classList.remove("sticky");
-  }
-}
-
-function change() {
-  var levelCbs = document.querySelectorAll(".level input[type='checkbox']");
-  var durationCbs = document.querySelectorAll(".duration input[type='checkbox']");
-  var filters = {
-    level: getClassOfCheckedCheckboxes(levelCbs),
-    duration: getClassOfCheckedCheckboxes(durationCbs)
-  };
-
-  filterResults(filters);
-}
-
-function getClassOfCheckedCheckboxes(checkboxes) {
-  var classes = [];
-
-  if (checkboxes && checkboxes.length > 0) {
-    for (var i = 0; i < checkboxes.length; i++) {
-      var cb = checkboxes[i];
-
-      if (cb.checked) {
-        classes.push(cb.getAttribute("rel"));
-      }
-    }
-  }
-
-  return classes;
-}
-
-function filterResults(filters) {
-  var rElems = document.querySelectorAll(".result div");
-  var hiddenElems = [];
-
-  if (!rElems || rElems.length <= 0) {
-    return;
-  }
-
-  for (var i = 0; i < rElems.length; i++) {
-    var el = rElems[i];
-
-    if (filters.level.length > 0) {
-      var isHidden = true;
-
-      for (var j = 0; j < filters.level.length; j++) {
-        var filter = filters.level[j];
-
-        if (el.classList.contains(filter)) {
-          isHidden = false;
-          break;
-        }
-      }
-
-      if (isHidden) {
-        hiddenElems.push(el);
-      }
-    }
-
-    if (filters.duration.length > 0) {
-      var isHidden = true;
-
-      for (var j = 0; j < filters.duration.length; j++) {
-        var filter = filters.duration[j];
-
-        if (el.classList.contains(filter)) {
-          isHidden = false;
-          break;
-        }
-      }
-
-      if (isHidden) {
-        hiddenElems.push(el);
-      }
-    }
-  }
-
-  for (var i = 0; i < rElems.length; i++) {
-    rElems[i].style.display = "block";
-  }
-
-  if (hiddenElems.length <= 0) {
-    return;
-  }
-
-  for (var i = 0; i < hiddenElems.length; i++) {
-    hiddenElems[i].style.display = "none";
-  }
-}
+    wepAccordionFaqItemHeaders.forEach(wepAccordionFaqItemHeader => {
+        wepAccordionFaqItemHeader.addEventListener("click", event => {
 
 
+            const currentlyActiveWepAccordionFaqItemHeader = document.querySelector(".wep-accordion-faq-item-header.active");
+            if (currentlyActiveWepAccordionFaqItemHeader && currentlyActiveWepAccordionFaqItemHeader !== wepAccordionFaqItemHeader) {
+                currentlyActiveWepAccordionFaqItemHeader.classList.toggle("active");
+                currentlyActiveWepAccordionFaqItemHeader.nextElementSibling.style.maxHeight = 0;
+            }
+
+            wepAccordionFaqItemHeader.classList.toggle("active");
+            const wepAccordionFaqItemBody = wepAccordionFaqItemHeader.nextElementSibling;
+            if (wepAccordionFaqItemHeader.classList.contains("active")) {
+                wepAccordionFaqItemBody.style.maxHeight = wepAccordionFaqItemBody.scrollHeight + "px";
+            }
+            else {
+                wepAccordionFaqItemBody.style.maxHeight = 0;
+            }
+
+        });
+    });
